5b18c4c8033faf111eb4e315b21c42ba1896a9a2
Syncla <vvazir@udel.edu>
2018-02-21 00:08:51 -0500

    Finished Compiler All functionality is in place, just need to neaten it up with comments and docstrings
    Signed-off-by: Syncla <vvazir@udel.edu>

--- lab1/calcC.c
+++ lab1/calcC.c
@@ -19,11 +19,17 @@ FILE * in;
 // Output file handle
 FILE * out;
 
+// Arrays to hold stripped instructions
+char* strpIns;
+char* strpArgs[4];
+
+
 //Function declerations
 int fileLen(FILE * fp);
 const char *getFilenameExt(const char *filename);
-void encode(char *line,char * output);
 void getOutputFile(const char *filename,char* outputFilename);
+void encode(char *args[],char[9]);
+void stripInstruction(char** arg,char* inputStr);
 
 void main(int argc, char *argv[]) {
 	if (argc != 2 && argc !=3 ) {
@@ -92,16 +98,17 @@ void main(int argc, char *argv[]) {
 				if (debug) {
 					printf("%s\n", lines[i]);
 				}
-
+				// Parse string and seperate instructions
+				stripInstruction(strpArgs,lines[i]);
 				// Convert to binary format
-				encode(lines[i],bin);
+				encode(strpArgs,bin);
 				// Write to output file
 				fprintf(out, bin,9);
 				// Uncomment this line for human read-ability
-				fprintf(out, "\n");
+				//fprintf(out, "\n");
 				// Print out encoded line
 				if (debug) {
-					printf("%s\n\n", lines[i]);
+					printf("\n");
 				}
 			}
 		}
@@ -117,23 +124,227 @@ void main(int argc, char *argv[]) {
 	// The code should never reach here, but just incase, say something went wrong
 	printf("Error while compiling\n");
 }
+void encode(char *args[], char out[9]){
+		
+		//Set up variables		
+		char cmdi[4];	//command in
+		char cmdo[2];	//command out
 
-void encode(char* line, char * output) {
-	/*
-		This function consumes a string of commands and produces a
-		binary representation of the string in the calc ISA
-		parameters:
-			line - a char array containg calc ISA commands
-			output - a char array to store the binary representation of the command
-		returns:
-	*/
-	line[1] = '!';
-	for (int i = 0; i < 8; i++) {
-		output[i] = line[i];
-	}
-	output[8] = '\0';
-	return;
-}
+		char rg0i[3];	//destination register in
+		char rg0o[3];	//destination register out
+		
+		char rg1i[3];	//source register in
+		char rg1o[3];	//source register out
+		
+		char rg2i[3];	//target register in
+		char rg2o[3];	//target register out
+		
+		char imm[5];	//immediate value
+		strcpy(imm,"none"); //initialize to "none"
+
+		char jmpi[2];	//jump amount in
+		char jmpo[3];	//jump amount out
+		
+		char instructionOut[9];		//binary instruction out
+		instructionOut[0] = '\0';	//start with null
+		
+		strcpy(cmdi, args[0]);
+		cmdi[3] = '\0';
+
+		strcpy(rg0i, args[1]);
+		rg0i[2] = '\0';
+
+		//Check the first register
+		
+		if((strcmp(rg0i,"r0") == 0)){
+			strcpy(rg0o,"00");
+		}//if
+
+		else if((strcmp(rg0i,"r1") == 0)){
+			strcpy(rg0o,"01");
+		}//if
+
+		else if((strcmp(rg0i,"r2") == 0)){
+			strcpy(rg0o,"10");
+		}//if	
+		
+		else if((strcmp(rg0i,"r3") == 0)){
+			strcpy(rg0o,"11");
+		}//if
+		
+		//check the command
+		
+		if ((strcmp(cmdi, "lod") == 0)){
+			strcpy(cmdo,"00");
+		}//if
+		
+		else if((strcmp(cmdi, "add") == 0)){
+			strcpy(cmdo,"01");
+		}//else if
+		
+		else if((strcmp(cmdi, "sub") == 0)){
+			strcpy(cmdo,"10");
+		}//else if
+		
+		else if((strcmp(cmdi, "dsp") == 0)){
+			strcpy(cmdo,"11");
+		}//else if
+		
+		else if((strcmp(cmdi, "cmp") == 0)){
+			strcpy(cmdo,"11");
+		}//else if
+		
+		//reading and converting now base on instruction
+		
+		if ((strcmp(cmdi,"lod") == 0)){			//lod
+			strcpy(imm, args[2]);
+			imm[5] = '\0';
+		
+			strcat(instructionOut,cmdo);
+			strcat(instructionOut,rg0o);
+			strcat(instructionOut,imm);
+			instructionOut[8] = '\0';
+		}//if load case
+		
+		else if ((strcmp(cmdi, "add") == 0) || (strcmp(cmdi, "sub") == 0)){ 	//add or sub
+			strcpy(rg1i, args[2]);
+			rg1i[2] = '\0';
+			
+			strcpy(rg2i, args[3]);
+			rg2i[2] = '\0';
+			
+			//read in 2nd register
+			
+			if((strcmp(rg1i,"r0") == 0)){
+				strcpy(rg1o,"00");
+			}//if
+
+			else if((strcmp(rg1i,"r1") == 0)){
+				strcpy(rg1o,"01");
+			}//if
+
+			else if((strcmp(rg1i,"r2") == 0)){
+				strcpy(rg1o,"10");
+			}//if	
+		
+			else if((strcmp(rg1i,"r3") == 0)){
+				strcpy(rg1o,"11");
+			}//if
+
+			//read in 3rd register
+			
+			if((strcmp(rg2i,"r0") == 0)){
+				strcpy(rg2o,"00");
+			}//if
+
+			else if((strcmp(rg2i,"r1") == 0)){
+				strcpy(rg2o,"01");
+			}//else if
+
+			else if((strcmp(rg2i,"r2") == 0)){
+				strcpy(rg2o,"10");
+			}//else if	
+		
+			else if((strcmp(rg2i,"r3") == 0)){
+				strcpy(rg2o,"11");
+			}//else if
+			
+			strcat(instructionOut,cmdo);
+			strcat(instructionOut,rg0o);
+			strcat(instructionOut,rg1o);
+			strcat(instructionOut,rg2o);		
+		
+		}//else if add or sub
+		
+		else if ((strcmp(cmdi, "cmp") == 0)){ 	//cmp
+			strcpy(rg1i, args[2]);
+			rg1i[2] = '\0';
+
+			//read in 2nd register
+			
+			if((strcmp(rg1i,"r0") == 0)){
+				strcpy(rg1o,"00");
+			}//if
+
+			else if((strcmp(rg1i,"r1") == 0)){
+				strcpy(rg1o,"01");
+			}//if
+
+			else if((strcmp(rg1i,"r2") == 0)){
+				strcpy(rg1o,"10");
+			}//if	
+		
+			else if((strcmp(rg1i,"r3") == 0)){
+				strcpy(rg1o,"11");
+			}//if
+
+			
+			strcpy(jmpi, args[3]);
+			jmpi[1] = '\0';
+			
+			//checking for number of lines to skip
+			if ((strcmp(jmpi,"1") == 0)){
+				strcpy(jmpo,"01");
+			}//if
+			
+			else if((strcmp(jmpi,"2") == 0)){
+				strcpy(jmpo,"10");
+			}//else if
+			
+			strcat(instructionOut,cmdo);
+			strcat(instructionOut,rg0o);
+			strcat(instructionOut,rg1o);
+			strcat(instructionOut,jmpo);		
+			
+		}//else if cmp
+		
+		
+		else if ((strcmp(cmdi, "dsp") == 0)){ 	//dsp
+			strcat(instructionOut,cmdo);
+			strcat(instructionOut,rg0o);
+			strcat(instructionOut,"0000");		
+			
+		}//else if dsp
+		
+		/*
+		printf("Command in: %s\n",cmdi);
+		printf("Register in: %s\n\n", rg0i);
+
+		printf("Command out: %s\n",cmdo);
+		printf("Register out: %s\n\n", rg0o);
+
+		printf("imm is: %s\n\n", imm);
+		*/
+		for (int i =0;i<9;i++){
+			out[i]=instructionOut[i];
+		}
+		if (debug){
+			printf("Final binary is: %s\n",instructionOut);
+		}
+		
+}//decode()
+
+
+void stripInstruction(char** arg, char* inputStr){
+		  char * instr;
+		  int index = 0;
+		  
+		  instr = strtok (inputStr," ,.-");
+		  
+		  while (instr != NULL){
+			arg[index] = instr;
+			instr = strtok (NULL, " ,.-");
+			index++;
+		  }//while
+		  
+		  while (index < 4){
+				arg[index] = " ";
+				index++;
+		  }//while
+
+}//strip
+
+
 
 const char *getFilenameExt(const char *filename) {
 	/*
