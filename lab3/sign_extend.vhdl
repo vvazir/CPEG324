403901e016f5fdefeb7e0d50eb0554eec0670d08
Jason Reynolds <jkr@udel.edu>
2018-04-21 21:26:09 -0400

    sign extend done

--- lab3/sign_extend.vhdl
+++ lab3/sign_extend.vhdl
@@ -3,24 +3,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity sign_extend is 
     port(
-        input:        in  std_logic_vector(7 downto 0);
+        input:        in  std_logic_vector(3 downto 0);
         output:        out std_logic_vector(7 downto 0)
         );
 end sign_extend;
 
 architecture behavioral of sign_extend is
 
-signal passThrough: std_logic_vector(3 downto 0);
-signal extended: std_logic;
-
-
 begin
-    extended <= input(3);
-    passThrough <= input(3 downto 0);
-    output(7)<= extended;
-    output(6)<= extended;
-    output(5)<= extended;
-    output(4)<= extended;
-    output(3 downto 0) <= passThrough;
+    output(7)<= input(3);
+    output(6)<= input(3);
+    output(5)<= input(3);
+    output(4)<= input(3);
+    output(3 downto 0) <= input(3 downto 0);
 
 end behavioral;
