25c269629f2cbc609a704564142b899e4c202fd0
Jason Reynolds <jkr@udel.edu>
2018-04-26 11:19:56 -0400

    added notebook for test cases

--- lab3/eightBitAdder_tb.vhdl
+++ lab3/eightBitAdder_tb.vhdl
@@ -33,38 +33,168 @@ signal Sum: std_logic_vector(7 downto 0);
 
 --stimulus
 process
-type pattern_type is record
+begin
+    
+    wait for 100 ns;
+    
+    
+    A <= "00000000";
+    B <= "00000001";
+    Cin <= '0';
+    wait for 100 ns;
+    
+    --sum should be 0001
+    --carryout/overflow: 0
+    --underflow:0
+    
+    A <= "00000001";
+    wait for 100 ns;
+    
+    --sum should be 0010
+    
+    B <= "00000010";
+    wait for 100 ns;
+    
+    --sum should be 0011
+    
+    A <= "00000010";
+    wait for 100 ns;
+    
+    --sum should be 0100
+    
+    B <= "00000011";
+    wait for 100 ns;
+    
+    --sum should be 0101
+    
+    A <= "00000011";
+    wait for 100 ns;
+    
+    --sum should be 0110
+    
+    B <= "00000100";
+    wait for 100 ns;
+    
+    --sum should be 0111
+    
+    A <= "00000100";
+    wait for 100 ns;
+    
+    --sum should be 1000
+    
+    B <= "00000101";
+    wait for 100 ns;
+    
+    --sum should be 1001
+    
+    A <= "00000101";
+    wait for 100 ns;
+    
+    --sum should be 1010
+    
+    B <= "00000110";
+    wait for 100 ns;
+    
+    --sum should be 1011
+    
+    A <= "00000110";
+    wait for 100 ns;
+    
+    --sum should be 1100
+    
+    B <= "00000111";
+    wait for 100 ns;
+    
+    --sum should be 1101
+    
+    A <= "00000111";
+    wait for 100 ns;
+    
+    --sum should be 1110
+    
+    B <= "00001000";
+    wait for 100 ns;
+    
+    --sum should be 1111
+    
+    A <= "00001000";
+    wait for 100 ns;
+    
+    --sum should be 
+    
+    B <= "00001001";
+    wait for 100 ns;
+    
+    --sum should be 
 
---inputs
-A:          std_logic_vector(7 downto 0);
-B:          std_logic_vector(7 downto 0);
+    A <= "00001001";
+    wait for 100 ns;
+    
+    --sum should be 
+    
+    B <= "00001010";
+    wait for 100 ns;
+    
+    --sum should be 
 
---outputs
-Sum: std_logic_vector(7 downto 0); 
+    A <= "00001010";
+    wait for 100 ns;
     
-end record;
+    --sum should be 
     
-type pattern_array is array (natural range <>) of pattern_type;
-constant patterns : pattern_array :=
-(
-("11111110","11111110","11111100")
-);
+    B <= "00001011";
+    wait for 100 ns;
+    
+    --sum should be 
 
-    begin
-    for n in patterns'range loop
+    A <= "00001011";
+    wait for 100 ns;
     
-    A<=patterns(n).A;
-    B<=patterns(n).B;
-    wait for 1 ns;
+    --sum should be 
     
-    assert Sum = patterns(n).Sum;
+    B <= "00001100";
+    wait for 100 ns;
     
-    report "bad output value" severity error;
-    end loop;
-    assert false report "end of test" severity note;
+    --sum should be 
+
+    A <= "00001100";
+    wait for 100 ns;
     
+    --sum should be 
+    
+    B <= "00001101";
+    wait for 100 ns;
+    
+    --sum should be 
+
+    A <= "00001101";
+    wait for 100 ns;
+    
+    --sum should be 
+    
+    B <= "00001110";
+    wait for 100 ns;
+    
+    --sum should be 
+
+    A <= "00001110";
+    wait for 100 ns;
+    
+    --sum should be 
+    
+    B <= "00001111";
+    wait for 100 ns;
+    
+    --sum should be 
+
+    A <= "00001111";
+    wait for 100 ns;
+    
+    --sum should be 
+
+
     wait;
-    
+
     
     end process;
     
