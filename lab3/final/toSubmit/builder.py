1df66d5a6ea15940f013c5036826ed5e446ab797
Syncla <vvazir@udel.edu>
2018-05-01 20:53:04 -0400

    mac?

--- lab3/final/toSubmit/builder.py
+++ lab3/final/toSubmit/builder.py
@@ -25,10 +25,10 @@ components=[
 	"zeroCheck"
 ]
 for comp in components:
-	subprocess.run(['ghdl','-a','--ieee=standard',comp+ext],shell=True)
-subprocess.run(['python', 'compiler.py', testBench],shell = True)
-subprocess.run(['ghdl','-a','--ieee=standard',vhdlTB+ext],shell=True)
-subprocess.run(['ghdl','-e','--ieee=standard',vhdlTB],shell=True)
-subprocess.run(['ghdl','-r','--ieee=standard',vhdlTB,'--vcd='+vcd],shell=True)
+	subprocess.run(['ghdl -a--ieee=standard {}'.format(comp+ext)],shell=True)
+subprocess.run(['python compiler.py {}'.format(testBench)],shell = True)
+subprocess.run(['ghdl -a --ieee=standard {}'.format(vhdlTB+ext)],shell=True)
+subprocess.run(['ghdl -e --ieee=standard {}'.format(vhdlTB)],shell=True)
+subprocess.run(['ghdl -r --ieee=standard {} --vcd={}'.format(vhdlTB,vcd)],shell=True)
 if (show):
-	subprocess.run(['gtkwave',vcd],shell=True)
+	subprocess.run(['gtkwave {}'.format(vcd)],shell=True)
